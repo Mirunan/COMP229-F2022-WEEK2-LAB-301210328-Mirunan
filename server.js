2f067b5dcd194d23dcff45c260dc9912e003d96a
Mirunan <mirunan999@gmail.com>
2022-09-14 02:35:51 -0400

    Instructions
    Nodejs application that listens to HTTP requests on port 3030 and return 3 different HTTP responses

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const connect = require('connect');
 const app = connect();
 
-app.use('/');
+function function_one(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from NodeJS Application');
+};
 
-app.listen(3000);
+function function_two(req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Hello from NodeJS Application as html</h1>');
+};
+
+function function_three(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end('{ “message”: “Hello from NodeJS Application as json”}');
+};
+
+
+app.use('/html', function_two);
+app.use('/json', function_three);
+app.use('/', function_one)
+app.listen(3030);
